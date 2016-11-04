19d3262fc8df4de8db7e480e4e8283c3af9ced8d
Dustin Van Tate Testa <toast27@gmail.com>
2016-11-04 10:41:39 -0400

    Add files via upload

---
+++ Ch4Proj2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+
+class Ch4Proj2 {
+	public static void main(String[] arg){
+
+		// why can't I just read directly from /dev/stdin ?
+		Scanner stdin = new Scanner(System.in);
+
+		System.out.print("How many sides go on the dice?  ");
+		Die player = new Die(stdin.nextInt()); // instanciate a dice obj
+
+
+		for (;;) {
+			System.out.println("The die has rolled a " + player.roll());
+
+			System.out.print("Go again? (Y/n) ");
+			for (;;) {
+				char input = stdin.next().charAt(0);
+				if (input == 'y' || input == 'Y') {
+					break;
+				} else if (input == 'n' || input == 'N')
+					return;
+				else
+					System.out.print("Invalid input, try again (Y/n): ");
+			}
+		}
+
+
+
+	}
+};
+
