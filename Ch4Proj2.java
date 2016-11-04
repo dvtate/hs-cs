51e5ab9aa842826f178ce12bf88ce39fc4a2739f
Dustin Van Tate Testa <toast27@gmail.com>
2016-11-04 12:57:37 -0400

    comments and shit

--- Ch4Proj2.java
+++ Ch4Proj2.java
@@ -11,15 +11,18 @@ class Ch4Proj2 {
 		Die player = new Die(stdin.nextInt()); // instanciate a dice obj
 
 
+		// main loop
 		for (;;) {
+
 			System.out.println("The die has rolled a " + player.roll());
 
 			System.out.print("Go again? (Y/n) ");
+			// ask the user for a response until they get their act together
 			for (;;) {
 				char input = stdin.next().charAt(0);
-				if (input == 'y' || input == 'Y') {
+				if (input == 'y' || input == 'Y')
 					break;
-				} else if (input == 'n' || input == 'N')
+				else if (input == 'n' || input == 'N')
 					return;
 				else
 					System.out.print("Invalid input, try again (Y/n): ");
@@ -30,4 +33,3 @@ class Ch4Proj2 {
 
 	}
 };
-
