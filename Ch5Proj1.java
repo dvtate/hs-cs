393ae2ddbce9056e4c524025d466a63f5412ee7b
Dustin Van Tate Testa <toast27@gmail.com>
2017-02-02 18:08:03 +0000

    random card generator driver calss

---
+++ Ch5Proj1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Ch5Proj1 {
+
+	public static void main(String[] args){
+
+		Card card = new Card();
+		for (int i = 0; i < 20; i++)
+			card.random().print();
+
+	}
+
+};
