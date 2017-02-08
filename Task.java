c14994a7fa76361e0ecf015b75b6161fa3ad4185
Dustin Van Tate Testa <toast27@gmail.com>
2017-02-07 23:08:57 -0500

    add more shit

--- Task.java
+++ Task.java
@@ -1,10 +1,29 @@
 import java.util.*;
 
+// something we need to do
+public class Task implements Priority {
+	public int priority;		// how important it is
+	public String name;			// what is it?
+	public boolean completed;	// are we done yet
 
-public class Task implements Priority, Named {
-	public int priority;
-	public String name;
-	public boolean completed;
+	// constructors
+	public Task(){
+		completed = false;
+	}
+	public Task(String taskName, int need, boolean done){
+		name = taskName;
+		priority = need;
+		completed = done;
+	}
+	public Task(String taskName, int need){
+		name = taskName;
+		priority = need;
+		completed = false;
+	}
+
+
+	// these aren't really needed but I did them anyway :P
+
 	public final void setPriority(int desireToComplete){
 		priority = desireToComplete;
 	}
@@ -12,21 +31,21 @@ public class Task implements Priority, Named {
 		return priority;
 	}
 	
-	public Task(){
-		completed = false;
-	}
-
-	public Task(String taskName, int need, boolean done){
-		name = taskName;
-		priority = need;
-		completed = done;
-	}
-
 	public final void setName(String newName){
 		name = newName;
 	}
-
 	public final String getName(){
 		return name;
 	}
+
+	public final void setCompleted(boolean done){
+		completed = done;
+	}
+	public final void finish(){
+		completed = true;
+	}
+	public final boolean isCompleted(){
+		return completed;
+	}
+		
 };
